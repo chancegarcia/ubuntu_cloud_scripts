700f1d3b9c72ab7950dbbbbf60e7bb7d749cf502
chance garcia <chance@chancegarcia.com>
2012-04-27 18:21:46 -0400

    last changes for running on rackspace

--- ruby_on_rails/setup_gem.sh
+++ ruby_on_rails/setup_gem.sh
@@ -4,9 +4,11 @@ wget http://rubyforge.org/frs/download.php/76011/rubygems-1.8.22.tgz
 tar -xvzf rubygems-1.8.22.tgz
 cd rubygems-1.8.22/
 ruby setup.rb
+sudo su
 export REALLY_GEM_UPDATE_SYSTEM=true
-gem update --system
-gem install rubygems-update
+gem1.9.1 update --system
+gem1.9.1 install rubygems-update
+ln -nfs /usr/bin/gem1.9.1 /usr/bin/gem
 sudo update_rubygems
 #ln -s /etc/alternatives/gem /usr/bin/gem-alt
-#ln -nfs /usr/bin/gem1.9.1 /usr/bin/gem
+
--- ruby_on_rails/setup_ror_dependencies.sh
+++ ruby_on_rails/setup_ror_dependencies.sh
@@ -4,7 +4,7 @@
 # update to latest ruby if needed
 apt-get install ruby1.9.1 rails nodejs libcurl4-openssl-dev libssl-dev zlib1g-dev apache2-prefork-dev libapr1-dev libaprutil1-dev libmysqlclient-dev libmysql-ruby1.9 ruby1.9.1-dev
 
-sudo update-alternatives --install /usr/bin/ruby ruby /usr/bin/ruby1.8 500 \
+update-alternatives --install /usr/bin/ruby ruby /usr/bin/ruby1.8 500 \
     --slave /usr/share/man/man1/ruby.1.gz ruby.1.gz \
             /usr/share/man/man1/ruby1.8.1.gz \
     --slave /usr/bin/ri ri /usr/bin/ri1.8 \
@@ -13,15 +13,15 @@ sudo update-alternatives --install /usr/bin/ruby ruby /usr/bin/ruby1.8 500 \
     --slave /var/lib/gems/bin gem-bin /var/lib/gems/1.8/bin
 
 
-sudo update-alternatives --install /usr/bin/ruby ruby /usr/bin/ruby1.9.1 400 \
+update-alternatives --install /usr/bin/ruby ruby /usr/bin/ruby1.9.1 400 \
     --slave /usr/share/man/man1/ruby.1.gz ruby.1.gz \
             /usr/share/man/man1/ruby1.9.1.1.gz \
     --slave /usr/bin/ri ri /usr/bin/ri1.9.1 \
     --slave /usr/bin/irb irb /usr/bin/irb1.9.1 \
     --slave /usr/bin/gem gem /usr/bin/gem1.9.1
     
-sudo update-alternatives --config ruby
-sudo update-alternatives --remove-all gem
+update-alternatives --config ruby
+update-alternatives --remove-all gem
 
 #ln -s /etc/alternatives/ruby /usr/bin/ruby-alt
 #ln -nsf /usr/bin/ruby1.9.1 /usr/bin/ruby
