d0b149be3526a9eba765336ab8477a7519b51d7d
chance garcia <develop@chancegarcia.com>
2012-04-27 14:24:24 -0400

    script updates

--- ruby_on_rails/setup_gem.sh
+++ ruby_on_rails/setup_gem.sh
@@ -4,3 +4,8 @@ wget http://rubyforge.org/frs/download.php/76011/rubygems-1.8.22.tgz
 tar -xvzf rubygems-1.8.22.tgz
 cd rubygems-1.8.22/
 ruby setup.rb
+gem update --system
+gem install rubygems-update
+sudo update_rubygems
+#ln -s /etc/alternatives/gem /usr/bin/gem-alt
+#ln -nfs /usr/bin/gem1.9.1 /usr/bin/gem
--- ruby_on_rails/setup_ror_passenger.sh
+++ ruby_on_rails/setup_ror_passenger.sh
@@ -2,3 +2,5 @@
 
 gem install passenger
 passenger-install-apache2-module
+#ln -nfs /var/lib/gems/1.9.1/gems/passenger-3.0.12/ /usr/lib/phusion-passenger
+#ln -nfs /var/lib/gems/1.9.1/gems/passenger-3.0.12/ /usr/share/phusion-passenger
