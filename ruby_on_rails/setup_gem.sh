7c3fbccc5e7bbe0ed7904f0eff50b6efa68b512d
chance garcia <chance@chancegarcia.com>
2012-04-27 15:10:02 -0400

    really update gem system

--- ruby_on_rails/setup_gem.sh
+++ ruby_on_rails/setup_gem.sh
@@ -4,6 +4,7 @@ wget http://rubyforge.org/frs/download.php/76011/rubygems-1.8.22.tgz
 tar -xvzf rubygems-1.8.22.tgz
 cd rubygems-1.8.22/
 ruby setup.rb
+export REALLY_GEM_UPDATE_SYSTEM=true
 gem update --system
 gem install rubygems-update
 sudo update_rubygems
--- ruby_on_rails/setup_ror_dependencies.sh
+++ ruby_on_rails/setup_ror_dependencies.sh
@@ -2,7 +2,7 @@
 
 # run as sudo
 # update to latest ruby if needed
-apt-get install ruby1.9.1 rails nodejs ruby-rvm libcurl4-openssl-dev libssl-dev zlib1g-dev apache2-prefork-dev libapr1-dev libaprutil1-dev libmysqlclient-dev libmysql-ruby1.9 ruby1.9.1-dev
+apt-get install ruby1.9.1 rails nodejs libcurl4-openssl-dev libssl-dev zlib1g-dev apache2-prefork-dev libapr1-dev libaprutil1-dev libmysqlclient-dev libmysql-ruby1.9 ruby1.9.1-dev
 
 sudo update-alternatives --install /usr/bin/ruby ruby /usr/bin/ruby1.8 500 \
     --slave /usr/share/man/man1/ruby.1.gz ruby.1.gz \
