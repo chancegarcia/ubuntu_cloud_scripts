49ebc4cd3c49c52b3ade98296e85f8c5df3828d9
chance garcia <develop@chancegarcia.com>
2013-02-15 14:39:51 -0500

    more setup scripts and a logrotate for auth log

---
+++ lampsetup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# install lamp
+apt-get install tasksel
+tasksel install lamp-server
+
+# install git/github things
+apt-get install xclip git
---
+++ usersetup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# create groups
+groupadd admin
+groupadd developer
+
+# add user
+useradd -c "Chance Garcia" -m -s "/bin/bash" chance
+
+# add to groups
+usermod -a -G admin,developer,sudo chance
+
+# prompt for password for new user
+passwd chance
